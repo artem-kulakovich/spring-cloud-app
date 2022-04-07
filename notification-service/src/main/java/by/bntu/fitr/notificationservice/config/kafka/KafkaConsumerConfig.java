7262e86dc4d537c256635e029db7a552db758dc5
=artem-kulakovich <=artemka228-1@mail.ru>
2022-04-07 21:58:57 +0300

    test class for kafka

--- notification-service/src/main/java/by/bntu/fitr/notificationservice/config/kafka/KafkaConsumerConfig.java
+++ notification-service/src/main/java/by/bntu/fitr/notificationservice/config/kafka/KafkaConsumerConfig.java
@@ -3,15 +3,12 @@ package by.bntu.fitr.notificationservice.config.kafka;
 import by.bntu.fitr.notificationservice.dto.EmailDTO;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
-import org.springframework.kafka.core.DefaultKafkaProducerFactory;
-import org.springframework.kafka.core.ProducerFactory;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
 
 import java.util.HashMap;
--- notification-service/src/main/java/by/bntu/fitr/notificationservice/controller/NotificationController.java
+++ notification-service/src/main/java/by/bntu/fitr/notificationservice/controller/NotificationController.java
@@ -3,12 +3,7 @@ package by.bntu.fitr.notificationservice.controller;
 import by.bntu.fitr.notificationservice.dto.EmailDTO;
 import by.bntu.fitr.notificationservice.service.SenderService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
